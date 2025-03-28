74a399041a1fe5ab839062fa07c09cd1c97f8455
Sameerdev512 <sameerkhatridev512@gmail.com>
2025-03-28 11:21:27 +0530

    jay shree ganesh

---
+++ src/test/java/com/mindsync/lostandfound/lost_and_found_backend/LostAndFoundBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindsync.lostandfound.lost_and_found_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostAndFoundBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
